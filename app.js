734b0d1e4dfda849ab32c13fae35921fc1fce464
amanfarhan2008@gmail.com <amanfarhan2008@gmail.com>
2025-07-02 11:11:44 +0500

    Javascript class added

---
+++ app.js
@@ -0,0 +1,141 @@
+// function doctorTime (time) {
+//     console.log("morning shift in your  " + time );
+//     console.log("evening shift in your  " + time );
+// }
+// doctorTime("torab")
+// doctorTime("siddique")
+
+// function hire (cook) {
+//     console.log(cook);
+// }
+// console.log("you give him breakfast biryani")
+// console.log("you give him dinner dal chawal")
+
+
+// logical operators 
+// let a = 6;
+// let b = 5;
+// let cond1 = a > b; // true 
+// let cond2 = a === 6; // true 
+// console.log( !(a > b ));
+
+
+
+// conditional statement 
+
+// let mode = "dark";
+// let color ;
+
+// if (mode === "dark") {
+//     color = "black";
+// }
+
+// if (mode === "light")  {
+//     color = "white"
+// }
+// console.log(color)
+
+
+
+// let age = 19 ;
+
+// if (age >= 18) {
+//     console.log( "you can vote")
+// }
+
+// if (age < 18) {
+//     console.log("you cannot vote")
+// }
+
+
+// let mode = "dark";
+// let color ;
+
+// if (mode === "dark") {
+//     color = "black";
+// }
+
+// if (mode === "light") {
+//     color = "white";
+// }
+
+
+// console.log(color)
+
+
+// let papa = "imran";
+// let father;
+ 
+// if (papa === "farhan") {
+//     father = "Aman";
+// }
+
+// if (papa === "imran") {
+//     father = "shuja";
+// }
+
+// console.log("papa he " + father +" ke")
+
+
+
+// let age = 17;
+
+// if (age >= 18){
+//     console.log("vote")
+// }  else {
+//     console.log("can not vote")
+// }
+
+
+// let age = 17;
+
+// if (age >= 18) {
+//     console.log("you can vote" );
+// }  else {
+//     console.log("you can not vote")
+// }
+// console.log("shik shak se phely mene thaks dekha han tifen box se phely mene druxe dekha ")
+
+
+// let num1 = 10;
+// let num = num1*2;
+
+// if (num % 2 === 0) {
+//     console.log( num +" is even")
+// }  else {
+//     console.log(num +" is odd")
+// }
+
+ 
+// let num = 15 ;
+//  if (num % 2 === 0 ) {
+//     console.log(num + " is even");
+//  }  else {
+//     console.log(num + " is odd");
+//  };
+
+// let age = 18;
+
+// if (age >= 18){
+//    console.log("you can vote");
+// } else {
+//    console.log("you can not vote")
+// }
+
+
+// let num = 20 ;
+
+// if (num % 2 === 0){
+//    console.log("even")
+// } else {
+//    console.log("odd")
+// }
+
+
+let num = 10;
+
+if (num % 2 === 0){
+   console.log("even")
+} else{
+   console.log("odd")
+}
